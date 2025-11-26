46b7758262947d201ac60d88c6e6c2fd78f0e6d3
black-hobo-box <schweitzer.fabian@gmail.com>
2025-11-26 13:32:47 +0100

    no with actual functionality...

--- workflow/run_snakemake.sh
+++ workflow/run_snakemake.sh
@@ -12,16 +12,16 @@ source ${HOME}/.bashrc
 source ${HOME}/miniconda3/etc/profile.d/conda.sh
 conda activate snakemake
 
-[ -d "${MIS}/workflow/samples"] || mkdir -p "${MIS}/workflow/samples"
-[ -d "${MIS}/workflow/ref"] || mkdir -p "${MIS}/workflow/ref"
+[ -d "${MIS}/workflow/samples" ] || mkdir -p "${MIS}/workflow/samples"
+[ -d "${MIS}/workflow/ref" ] || mkdir -p "${MIS}/workflow/ref"
 if [ ! -f "${MIS}/workflow/ref/GCF_001465965.1_Pdom_r1.2_genomic.fna" ]; then
     cp "${MIS}/GCF_001465965.1_Pdom_r1.2_genomic.fna" "${MIS}/workflow/ref/GCF_001465965.1_Pdom_r1.2_genomic.fna"
 fi
 if [ ! -f "${MIS}/workflow/samples/AAA9495_1.fq.gz" ]; then
-    cp "${MIS}/AAA9495_?.fq.gz" "${MIS}/workflow/samples/"
+    cp ${MIS}/AAA9495_?.fq.gz "${MIS}/workflow/samples/"
 fi
 if [ ! -f "${MIS}/workflow/samples/AAB7105_1.fq.gz" ]; then
-    cp "${MIS}/AAA9495_?.fq.gz" "${MIS}/workflow/samples/"
+    cp ${MIS}/AAB7105_?.fq.gz "${MIS}/workflow/samples/"
 fi
 
 cd ${MIS}/workflow
